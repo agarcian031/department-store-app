8ba7d5813f749adb40520d42eec7696d68b36efd
agarcian031 <algarcian@outlook.com>
2019-05-15 15:46:32 -0600

    Items and Departments, CRUD actions for each

---
+++ app/controllers/departments_controller.rb
@@ -0,0 +1,52 @@
+class DepartmentsController < ApplicationController
+  before_action :set_department, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @departments = Department.all 
+  end
+
+  def show
+  end
+
+  def new
+    @department = Department.new 
+    render partial: "form"
+  end
+
+  def create
+    @department = Department.new(departments_params)
+
+    if @department.save 
+      redirect_to departments_path 
+    else 
+      render partial: "form" 
+    end 
+  end 
+
+  def edit
+    render partial: "form"
+  end
+
+  def update 
+
+    if @department.update(departments_params)
+      redirect_to departments_path 
+    else
+      render partial: "form"
+    end 
+  end 
+
+  def destroy
+    @department.destroy 
+    redirect_to departments_path
+  end 
+
+  private 
+  def departments_params 
+    params.require(:department).permit(:name)
+  end 
+
+  def set_department 
+    @department = Department.find(params[:id])
+  end 
+end
